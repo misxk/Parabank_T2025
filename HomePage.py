562929d692706fed68cc17e78e0f1024df784c6c
misxk <jakubczykmichal99@gmail.com>
2025-03-01 19:01:36 +0100

    1st commit

--- HomePage.py
+++ HomePage.py
@@ -2,6 +2,6 @@ class HomePage:
     def __init__(self, page):
         self.page = page
     def goto(self):
-        self.page.goto("https://allegro.pl")
+        self.page.goto("https://parabank.parasoft.com/parabank/index.htm")
     def get_title(self):
         return self.page.title()

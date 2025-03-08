57017bdec04206b6040feecf6867903068e998b4
misxk <jakubczykmichal99@gmail.com>
2025-03-08 12:30:27 +0100

    11th commit

--- classes/register_page.py
+++ classes/register_page.py
@@ -16,30 +16,30 @@ class RegistrationPage():
 
     def register_user(self):
         # generating data
-        first_name = self.fake.first_name()
-        last_name = self.fake.last_name()
-        address = self.fake.address().split('\n')[0]
-        city = self.fake.city()
-        state = self.fake.state()
-        zip_code = self.fake.zipcode()
-        phone_number = self.fake.phone_number()
-        ssn = self.fake.ssn()
-        self.username = self.fake.user_name()  # Przechowywanie wygenerowanego username
-        self.password = self.fake.password()  # Przechowywanie wygenerowanego password
+        first_name = self.fake.first_name() # generating first name
+        last_name = self.fake.last_name() # generating last name
+        address = self.fake.address().split('\n')[0] # generating address
+        city = self.fake.city() # generating city
+        state = self.fake.state() # generating state
+        zip_code = self.fake.zipcode() # generating zip code
+        phone_number = self.fake.phone_number() # generating phone number
+        ssn = self.fake.ssn() # generating ssn
+        self.username = self.fake.user_name()  # storing generated username
+        self.password = self.fake.password()  # storing generated password
 
         # filling the form
-        self.page.fill('[name="customer.firstName"]', first_name)
-        self.page.fill('[name="customer.lastName"]', last_name)
-        self.page.fill('[name="customer.address.street"]', address)
-        self.page.fill('[name="customer.address.city"]', city)
-        self.page.fill('[name="customer.address.state"]', state)
-        self.page.fill('[name="customer.address.zipCode"]', zip_code)
-        self.page.fill('[name="customer.phoneNumber"]', phone_number)
-        self.page.fill('[name="customer.ssn"]', ssn)
-        self.page.fill('[name="customer.username"]', self.username)  # Wykorzystanie wygenerowanego username
-        self.page.fill('[name="customer.password"]', self.password)  # Wykorzystanie wygenerowanego password
-        self.page.fill('[name="repeatedPassword"]', self.password)  # Wykorzystanie wygenerowanego password
-        self.page.click('input.button[value="Register"]')
+        self.page.fill('[name="customer.firstName"]', first_name)# using generated first name
+        self.page.fill('[name="customer.lastName"]', last_name) # using generated last name
+        self.page.fill('[name="customer.address.street"]', address) # using generated address
+        self.page.fill('[name="customer.address.city"]', city) # using generated city
+        self.page.fill('[name="customer.address.state"]', state) # using generated state
+        self.page.fill('[name="customer.address.zipCode"]', zip_code) # using generated zip code
+        self.page.fill('[name="customer.phoneNumber"]', phone_number) # using generated phone number
+        self.page.fill('[name="customer.ssn"]', ssn) # using generated ssn
+        self.page.fill('[name="customer.username"]', self.username)  # using generated username
+        self.page.fill('[name="customer.password"]', self.password)  # using generated passsword
+        self.page.fill('[name="repeatedPassword"]', self.password)  # repeat generated password
+        self.page.click('input.button[value="Register"]') # clicking register button
 
     def download_data(self):
         # returning generated data

b563e5d7adcf0305477ca9140e2e41ae6cdb441c
misxk <jakubczykmichal99@gmail.com>
2025-03-08 12:40:23 +0100

    12th commit

--- tests/register_page_test.py
+++ tests/register_page_test.py
@@ -10,21 +10,25 @@ from classes.driver import PlaywrightBrowser
 def browser():
     browser = PlaywrightBrowser()
     browser.launch_browser()
-    yield browser  # Returning the browser to tests
-    browser.close_browser()  # After tests closing the browser
+    yield browser  # returning the browser to tests
+    browser.close_browser()  # after tests closing the browser
 
+
+# test to check if user can register
 def test_to_check_if_user_can_register(browser):
+
     admin_page = AdminPage(browser.page)
     admin_page.goto()
     admin_page.click_admin_page()
     admin_page.click_clean_button()
+
     home_page = HomePage(browser.page)
     home_page.goto()
-    # Asserting if homepage is opened
+    # asserting if homepage is opened
     assert browser.page.url == 'https://parabank.parasoft.com/parabank/index.htm', 'Homepage is not opened.'
-    register_page = RegistrationPage(browser.page)
-    register_page.goto()
-    fake = Faker()
+    register_page = RegistrationPage(browser.page)  # creating register page
+    register_page.goto() # entering register page
+    fake = Faker() # creating fake data
     register_page.page.fill('[name="customer.firstName"]', fake.first_name()) # filling the form with fake first name
     register_page.page.fill('[name="customer.lastName"]', fake.last_name()) # filling the form with fake last name
     register_page.page.fill('[name="customer.address.street"]', fake.street_address()) # filling the form with fake address street
@@ -38,12 +42,12 @@ def test_to_check_if_user_can_register(browser):
     register_page.page.fill('[name="customer.password"]', password) # filling the form with fake password
     register_page.page.fill('[name="repeatedPassword"]', password) # repeating the password
     register_page.click_register_button() # clicking register button
-# Asserting if register page is opened
+    # asserting if register page is opened
     assert browser.page.url == 'https://parabank.parasoft.com/parabank/register.htm', 'Register page is not opened.'
-# Waiting for the page to load
+    # waiting for the page to load
     if not browser.page.is_closed():
         browser.page.wait_for_url('https://parabank.parasoft.com/parabank/register.htm')
-# Asserting if log out button is visible
+    # asserting if log out button is visible
     success_message = browser.page.locator("//div[@id='rightPanel']/p").text_content()
     assert success_message == 'Your account was created successfully. You are now logged in.'
 
